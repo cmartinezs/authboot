40517e1c9314d753cf6783f1c4c5511b5ed204af
Fernanda Garay <m.garay@duocuc.cl>
2023-10-26 00:32:59 -0300

    Agrega usuario
    Se agrega usuario y se modifica otro

--- infra/src/main/resources/sql_imports/05_user.sql
+++ infra/src/main/resources/sql_imports/05_user.sql
@@ -5,5 +5,6 @@ INSERT INTO users (username, password, email, created_at, updated_at, enabled_at
 VALUES ('fgaray', '$2a$12$xRtGrFGa9fLW5jTDC7wR8eVWCpaSLrc1p6YrGR0n.lj5kAtuyJhjC', 'fgaray@telopresto.cl', NOW(), NOW(), NOW()),
        ('rulloa', '$2a$12$Zy2njvB4i/Lae5LjQDuYp.Zv8wocuLCGpUtTOigR6fazC03xo5xLW', 'rulloa@telopresto.cl', NOW(), NOW(), NOW()),
        ('ctroncoso', '$2a$12$zCP.BU.CuPFBAk/dLVieQ.4.qm0lJXTI9TACu.Rbq7NpFj.12fA/6', 'ctroncoso@telopresto.cl', NOW(), NOW(), NOW()),
-       ('gabriel', '$2a$12$lURpB0tUwKaolMJEJGQqIO5k4WVqEzb.qk7T0od6PCOzWNhpPnFe6', 'gabriel@telopresto.cl', NOW(), NOW(), NOW()),
-       ('rentadmin', '$2a$12$UtGkealcaYvJ4bpsQXX69u/JzIm1JZ534QcY3yaGAxGzG7DTrJEpS', 'rentadmin@telopresto.cl', NOW(), NOW(), NOW());
+       ('gcampos', '$2a$12$N4WGJ5H9/xzMC3E7sggqquZJMBUZS1rQpqHVCCjjxE.m.j3wXwLoG', 'gcampos@telopresto.cl', NOW(), NOW(), NOW()),
+       ('rentadmin', '$2a$12$UtGkealcaYvJ4bpsQXX69u/JzIm1JZ534QcY3yaGAxGzG7DTrJEpS', 'rentadmin@telopresto.cl', NOW(), NOW(), NOW()),
+       ('rentventa', '$2a$12$9evaMpSbVky7HGRpzFBh9unr5ReaLtbZr2Zg1fSRZBFIlxenp9LSm', 'rentventa@telopresto.cl', NOW(), NOW(), NOW());
